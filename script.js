8395eba4a54a8c72464246f0654ab3a83780cb61
Aquib <mdaquibanwer@gmail.com>
2022-12-02 06:56:26 +0530

    completed tic tac toe

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ const changeTurn = ()=>{
 const checkWinner = ()=>{
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
+        [0,1,2,5,5,0],
+        [3,4,5,5,15,0],
+        [6,7,8,5,25,0],
+        [0,3,6,-5,15,90],
+        [1,4,7,5,15,90],
+        [2,5,8,15,15,90],
+        [0,4,8,5,15,45],
+        [2,4,6,5,15,135]
     ]
     wins.forEach(e=>{
         if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[1]].innerText === boxtext[e[2]].innerText) && (boxtext[e[0]].innerText !== "")){
@@ -33,8 +33,12 @@ const checkWinner = ()=>{
             gameWon=true;
             gameOver.play();
             document.querySelector(".imgbox").getElementsByTagName('img')[0].style.width = "250px"
+            document.querySelector(".line").style.width = "25vw";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw,${e[4]}vw) rotate(${e[5]}deg)`
         }
+        
     })
+
     
 }
 
@@ -52,7 +56,7 @@ resetbtn.addEventListener('click',()=>{
     gameTurn = "X";
     gameWon =false;
     document.getElementsByClassName("info")[0].innerText = "Turn For " + gameTurn;
-    
+    document.querySelector(".line").style.width = "0";
 })
 
 
